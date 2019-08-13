fe2197f41b8e38399136e16bc4e5c380e2f46687
Bennik2000 <bennik.ko@gmail.com>
2019-08-13 17:58:32 +0200

    Added databaes sanitizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:kiwi/kiwi.dart' as kiwi;
+import 'package:newsletter_reader/business/articles/database_article_sanitizer.dart';
 import 'package:newsletter_reader/system/periodic_background_task.dart';
 import 'package:newsletter_reader/ui/app.dart';
 import 'package:newsletter_reader/ui/dependency_injection.dart';
 
-void main() {
+Future main() async {
   inject();
+
+  await new DatabaseArticleSanitizer(
+    kiwi.Container().resolve(),
+    kiwi.Container().resolve(),
+  ).sanitize();
+
   runApp(MyApp());
   new PeriodicBackgroundTask().registerBackgroundTask();
 }
